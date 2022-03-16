45006b6c820be0e2653cb6337d6c8257383bac30
ekball <90882418+ekball@users.noreply.github.com>
2022-03-16 15:45:07 -0400

    added randomized pokemon image with correct matching name to the pokedex page front

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { randomize } from '../../utils/helpers';
+
+const GRAPHQL_API = "https://graphql-pokeapi.graphcdn.app/";
+const GET_POKEMON_QUERY = `query pokemons($limit: Int, $offset: Int) {
+            pokemons(limit: $limit, offset: $offset) {
+                count
+                results {
+                    url
+                    name
+                    image
+                }
+            }
+          }`;
 
 function Pokedex() {
+    
+    const [pokeImage, setPokeImage] = useState([]);
+    const [pokeName, setPokeName] = useState([]);
+
+    // choose a random number out of total existing pokemon
+    const randomNumber = randomize(1100)
+
+    // variables set up to use api
+        // return a single pokemon
+        // offset count from zero by the random number chosen above
+    const gqlVariables = {
+        limit: 1,
+        offset: randomNumber,
+    };
+
+    // fetch request to pull pokemon image
+    useEffect(() => {
+        fetch('https://graphql-pokeapi.graphcdn.app/', {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: GET_POKEMON_QUERY, variables: gqlVariables }),
+        })
+        .then(response => response.json())
+        .then(data => setPokeImage(data.data.pokemons.results[0].image))
+    },[])
+
+    // fetch request to pull pokemon image
+    useEffect(() => {
+        fetch('https://graphql-pokeapi.graphcdn.app/', {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: GET_POKEMON_QUERY, variables: gqlVariables }),
+        })
+        .then(response => response.json())
+        .then(data => setPokeName(data.data.pokemons.results[0].name))
+    },[])
+
     return (
-        <section className='dex-section'>
+        <div >
 
-            <h1 id="dex" className="dex-page-title">Pokedex Search</h1>
-
-            {/* incorporate apis here */}
-            {/* might need saving function to keep info of favorite pokemon for each user's profile */}
-
-        </section>
+            <img className='main-image' src={pokeImage} alt='pokemon'></img>
+            <p>{pokeName}</p>
+        
+        </div>
     );
-}
+}  
 
 export default Pokedex;
