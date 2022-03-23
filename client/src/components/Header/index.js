94bffc64ad4cc932629b4dac6d23dfb674c3a9fa
LexieHolder <lexieannholder@gmail.com>
2022-03-22 22:02:02 -0400

    progress on nav button orientation

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,17 +11,17 @@ const Header = () => {
       };
 
   return (
-    <header className="header-section w-full justify-items-center mx-auto px-4 sm:px-6">
+    <header className="header-section flex items-center justify-center mb-3">
       <img src={image} class="max-w-sm h-auto object-center mx-auto"/>
-      <div className="container flex-row justify-space-between-lg justify-center items-center">
-        
       
+      <div className="container flex-row justify-space-between-lg justify-center items-center mx-auto">
         
-        <Link to="/"className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Home</Link>
-        <Link to="/dex" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Pokédex</Link>
-        <Link to="/releases" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Release Info</Link>
-        <Link to="/chatroom" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Chatroom</Link>
-        <nav className="text-center">
+        <Link to="/"className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mx-auto'>Home</Link>
+        <Link to="/dex" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mx-auto'>Pokédex</Link>
+        <Link to="/releases" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mx-auto'>Release Info</Link>
+        <Link to="/chatroom" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mx-auto'>Chatroom</Link>
+        
+        <nav className="flex items-center justify-center mb-3">
             {Auth.loggedIn() ? (
                 <>
                 <Link to="/profile" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Profile</Link>
@@ -35,7 +35,7 @@ const Header = () => {
                 <Link to="/signup" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Signup</Link>
                 </>
             )}
-            </nav>
+        </nav>
       </div>
     </header>
   );
