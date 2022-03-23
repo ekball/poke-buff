2b18d524de2bc6e3c5da4045043dfeba105dc840
LexieHolder <lexieannholder@gmail.com>
2022-03-22 20:35:16 -0400

    fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,13 +3,15 @@ import React from "react";
 function Footer() {
 
     return(
-        <div className="footer-section flex flex-wrap">
-
-            Credit to Poke API for providing the dex information.
+        <footer className="footer-section flex flex-col bg-gray-200 text-center lg:text-left font-sans fixed bottom-0">
+            <p class="text-gray-700 text-center p-4">
+               Credit to Poke API for providing the dex information.
             Credit to RAWG API for providing the game release date information.
-            Credit to TPC™® for the usage of their images and information on the franchise.
+            Credit to TPC™® for the usage of their images and information on the franchise. 
+            </p>
+            
         
-        </div>
+        </footer>
     );
 }
 
