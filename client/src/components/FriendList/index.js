914d00d2083e248953f31ae399421b9b2f7e6cc6
ekball <90882418+ekball@users.noreply.github.com>
2022-03-23 00:48:17 -0400

    finished styling FriendList

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -7,12 +7,12 @@ const FriendList = ({ friendCount, username, friends }) => {
   }
 
   return (
-    <div>
-      <h5>
+    <div className='flex-row justify-center'>
+      <h5 className='flex justify-center rounded-t-lg bg-rose-500 w-full p-2'>
         {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
       </h5>
       {friends.map(friend => (
-        <button className="inline-block px-6 py-2.5 bg-gray-600 text-red-500 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out " key={friend._id}>
+        <button className="flex-row m-2 px-6 py-2 bg-slate-200 text-red-500 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg" key={friend._id}>
           <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
         </button>
       ))}
