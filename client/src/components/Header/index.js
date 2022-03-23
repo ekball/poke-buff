1daa210518fa5e540bb0ea13706fc29a5b9114aa
LexieHolder <lexieannholder@gmail.com>
2022-03-22 21:51:17 -0400

    exchanged pokebuff header title for image, used photoshop to blur edges to make less contrast clash

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
-
+import image from '../Home/pokebuff-title-blur.png';
 
 const Header = () => {
 
@@ -12,10 +12,11 @@ const Header = () => {
 
   return (
     <header className="header-section w-full justify-items-center mx-auto px-4 sm:px-6">
+      <img src={image} class="max-w-sm h-auto object-center mx-auto"/>
       <div className="container flex-row justify-space-between-lg justify-center items-center">
-        <Link to="/">
-          <h1 className='site-title flex items-center lg:w-0 lg:flex-1'>PokeBuff</h1>
-        </Link>
+        
+      
+        
         <Link to="/"className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Home</Link>
         <Link to="/dex" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Pokédex</Link>
         <Link to="/releases" className='nav-tab hover:animate-bounce rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-slate-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 '>Release Info</Link>
